576c226baa182c30fb7f48211450bf1c104af88b
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2018-02-20 17:09:08 -0500

    update __init__

--- jhu_primitives/ase/__init__.py
+++ jhu_primitives/ase/__init__.py
@@ -1,5 +1,5 @@
-__author__  = 'JHU Team'
-__version__ = '0.3.0'
-
 from __future__ import absolute_import
 from .ase import AdjacencySpectralEmbedding
+
+__author__  = 'JHU Team'
+__version__ = '0.3.0'
