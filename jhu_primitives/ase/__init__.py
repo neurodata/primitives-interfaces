07af841d1274f875a4759e3759b580c207cfe4a4
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2018-02-20 17:15:20 -0500

    another try.

--- jhu_primitives/ase/__init__.py
+++ jhu_primitives/ase/__init__.py
@@ -1,5 +1,12 @@
+"""
+    ase
+    __init__.py
+
+    @author: JHU Team
+    @version: 0.3.0
+
+    Adjacency Spectral Embedding
+"""
+
 from __future__ import absolute_import
 from .ase import AdjacencySpectralEmbedding
-
-__author__  = 'JHU Team'
-__version__ = '0.3.0'
