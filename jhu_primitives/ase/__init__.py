85ff7e199d01a977ff1ec815f73a0cd0de5aa85a
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2018-02-20 17:04:27 -0500

    rearrange __init__.py

--- jhu_primitives/ase/__init__.py
+++ jhu_primitives/ase/__init__.py
@@ -1,5 +1,5 @@
-from __future__ import absolute_import
-from .ase import AdjacencySpectralEmbedding
-
 __author__  = 'JHU Team'
 __version__ = '0.3.0'
+
+from __future__ import absolute_import
+from .ase import AdjacencySpectralEmbedding
