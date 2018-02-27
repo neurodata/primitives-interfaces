ba32f05bca3004e85455d5cd236742d59ce591ed
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2018-02-27 10:13:23 -0500

    add r-base-dev and r-recommended

--- install_r.sh
+++ install_r.sh
@@ -4,6 +4,6 @@ gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
 gpg -a --export E084DAB9 | apt-key add -
 sudo apt-get update
 sudo apt-get -y install r-base
-#sudo apt-get -y install r-base-dev
-#sudo apt-get -y install r-recommended
+sudo apt-get -y install r-base-dev
+sudo apt-get -y install r-recommended
 
