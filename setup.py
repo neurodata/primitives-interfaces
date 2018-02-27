c829a3e87fdb6a41b64a4f2f87e1b66e9d51ca1b
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2018-02-27 00:15:01 -0500

    trying install subclass.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from setuptools import setup
+from setuptools.command.install import install
 from subprocess import check_output, call
 from sys import platform
 
@@ -8,26 +9,34 @@ PACKAGE_NAME = 'jhu_primitives'
 MINIMUM_PYTHON_VERSION = 3, 6
 VERSION = '0.0.4'
 
+class InstallR(install):
+    #https://stackoverflow.com/questions/15440115/how-would-i-run-a-script-file-as-part-of-the-python-setup-py-install#16609054
+    def inr(self):
+        install.run(self)
+        print("\n\n\nInstalling R:\n\n\n")
+        subprocess.call("install_r.sh", shell=True)
+
+
 def check_python_version():
     """Exit when the Python version is too low."""
     if sys.version_info < MINIMUM_PYTHON_VERSION:
         sys.exit("Python {}.{}+ is required.".format(*MINIMUM_PYTHON_VERSION))
 
-def install_r():
-    print("Trying to install R")
-    sys.stdout.flush()
-    """ Install r-base using apt-get if on UBUNTU"""
-    if platform == "linux" or platform == "linux2":
-        ## https://cran.rstudio.com/bin/linux/ubuntu/
-        print("Adding rstudio repo for Artful")
-        sys.stdout.flush()
-        os.system("sh -c '''echo 'deb https://cran.rstudio.com/bin/linux/ubuntu artful/ >> /etc/apt/sources.list'''")
-        os.system("gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9")
-        os.system("gpg -a --export E084DAB9 | apt-key add -")
-        os.system("apt-get update")
-        os.system("apt-get -y install r-base")
-        #os.system("apt-get -y install r-base-dev")
-        #os.system("apt-get -y install r-recommended")
+#def install_r():
+#    print("Trying to install R")
+#    sys.stdout.flush()
+#    """ Install r-base using apt-get if on UBUNTU"""
+#    if platform == "linux" or platform == "linux2":
+#        ## https://cran.rstudio.com/bin/linux/ubuntu/
+#        print("Adding rstudio repo for Artful")
+#        sys.stdout.flush()
+#        os.system("sh -c '''echo 'deb https://cran.rstudio.com/bin/linux/ubuntu artful/ >> /etc/apt/sources.list'''")
+#        os.system("gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9")
+#        os.system("gpg -a --export E084DAB9 | apt-key add -")
+#        os.system("apt-get update")
+#        os.system("apt-get -y install r-base")
+#        #os.system("apt-get -y install r-base-dev")
+#        #os.system("apt-get -y install r-recommended")
 
 
 def read_package_variable(key):
@@ -42,7 +51,7 @@ def read_package_variable(key):
 
 check_python_version()
 
-install_r()
+#install_r()
 
 setup(
     name=PACKAGE_NAME,
@@ -82,6 +91,8 @@ setup(
     },
     package_data = {'': ['*.r', '*.R']},
     include_package_data = True,
+    scripts=['install_r.sh'],
+    cmdclass={'install': InstallR},
     install_requires=['typing', 'numpy', 'scipy',
         'python-igraph', 'rpy2', 'sklearn', 'jinja2', 'primitive_interfaces'],
     url='https://github.com/neurodata/primitives-interfaces',
