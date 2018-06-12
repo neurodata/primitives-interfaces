d819a94db9cd8c43414744762462dc8c58bbc1b8
Joshua Agterberg <34112007+jagterberg@users.noreply.github.com>
2018-06-12 17:13:30 -0400

    Update vnsgm.py

--- jhu_primitives/vnsgm/vnsgm.py
+++ jhu_primitives/vnsgm/vnsgm.py
@@ -8,11 +8,9 @@ from typing import Sequence, TypeVar, Union, Dict
 import os
 
 
-from primitive_interfaces.transformer import TransformerPrimitiveBase
+from d3m.primitive_interfaces.transformer import TransformerPrimitiveBase
 #from jhu_primitives.core.JHUGraph import JHUGraph
 import numpy as np
-
-
 from d3m import container
 from d3m import utils
 from d3m.metadata import hyperparams, base as metadata_module, params
