864f0f4cbcbe678f50add285143f3692d457b433
hhelm10 <31412718+hhelm10@users.noreply.github.com>
2018-06-14 09:35:32 -0400

    deleted file_path_conversion from dimselect

--- jhu_primitives/dimselect/dimselect.py
+++ jhu_primitives/dimselect/dimselect.py
@@ -11,6 +11,7 @@ import rpy2.robjects.numpy2ri
 rpy2.robjects.numpy2ri.activate()
 from d3m.primitive_interfaces.transformer import TransformerPrimitiveBase
 import numpy as np
+from scipy.stats import norm
 from d3m import utils, container
 from d3m.metadata import hyperparams, base as metadata_module, params
 from d3m.primitive_interfaces import base
@@ -25,32 +26,6 @@ class Params(params.Params):
 
 class Hyperparams(hyperparams.Hyperparams):
     n_elbows = hyperparams.Hyperparameter[int](default=3, semantic_types=['https://metadata.datadrivendiscovery.org/types/TuningParameter'])
-
-def file_path_conversion(abs_file_path, uri="file"):
-    local_drive, file_path = abs_file_path.split(':')[0], abs_file_path.split(':')[1]
-    path_sep = file_path[0]
-    file_path = file_path[1:]  # Remove initial separator
-    if len(file_path) == 0:
-        print("Invalid file path: len(file_path) == 0")
-        return
-
-    s = ""
-    if path_sep == "/":
-        s = file_path
-    elif path_sep == "\\":
-        splits = file_path.split("\\")
-        data_folder = splits[-1]
-        for i in splits:
-            if i != "":
-                s += "/" + i
-    else:
-        print("Unsupported path separator!")
-        return
-
-    if uri == "file":
-        return "file://localhost" + s
-    else:
-        return local_drive + ":" + s
 
 class DimensionSelection(TransformerPrimitiveBase[Inputs, Outputs, Hyperparams]):
     # This should contain only metadata which cannot be automatically determined from the code.
@@ -139,12 +114,83 @@ class DimensionSelection(TransformerPrimitiveBase[Inputs, Outputs, Hyperparams])
         }
         """ % path
 
-        #print(cmd)
-
         result = np.array(robjects.r(cmd)(inputs, n_elbows))
 
         outputs = container.ndarray(result)
 
-        if inputs is 
-
         return base.CallResult(outputs)
+
+        ### BEGIN - PYTHON IMPLEMENTATION
+
+        """
+        TODO: If a graph, use ASE...
+
+        if type(g) == numpy.ndarray:
+            if g.shape[0] == g.shape[1]: # n x n matrix
+                g = networkx.Graph(g) # convert to networkx graph to be able to extract edge list 
+            elif g.shape[1] == 2: # n x 2 matrix
+                g = igraph.Graph(list(g))
+            else:
+                print("Neither n x n nor n x 2. Please submit a square matrix or edge list.")
+                return
+                
+        if type(g) == networkx.classes.graph.Graph: # networkx graph
+            g = igraph.Graph(list(g.edges)) # convert to igraph graph, find the clusters
+
+		if inputs is <networkx.graph.Graph>
+
+		if inputs is <type(ase.produce.value[1]) or flat np.array>:
+			elbows = profile_likelihood_maximization(U, self.hyperparams['n_elbows'])
+		
+
+	def profile_likelihood_maximization(U, n_elbows, elbows_found = []):
+		"""
+		"""
+		Inputs
+			U - An ordered list of eigenvalues
+			n - The number of elbows to return
+
+		Return
+			elbows - An numpy array containing elbows
+
+		if len(elbows_found) == n_elbows:
+			return elbows_found
+
+		n = len(X)
+
+		if n == 0:
+			return container.ndarray(elbows_found)
+
+		if n == 1:
+			if len(elbows_found) == 0:
+				return U
+			else:
+				return container.ndarray(elbows_found.append(U[0] + elbows_found[-1]))
+
+    	d = 1
+    	sample_var = var(U, ddof = 1)
+    	sample_scale = sample_var**(1/2)
+    	elbow = 1 # Initialize elbow
+    	likelihood_elbow = 0
+    	likelihoods = np.array([])
+    	while d < n:
+        	mean_sig = mean(X[:d])
+        	mean_noise = mean(X[d:])
+        	sig_likelihood = 0
+        	noise_likelihood = 0
+        	for i in range(d):
+            	sig_likelihood += norm.pdf(X[i], mean_sig, sample_scale)
+        	for i in range(d, n):
+            	noise_likelihood += norm.pdf(X[i], mean_noise, sample_scale)
+            
+        	likelihood = noise_likelihood + sig_likelihood
+        	likelihoods.append(likelihood)
+        
+        	if likelihood > likelihood_elbow:
+            	likelihood_elbow = likelihood 
+            	elbow = d
+        	d += 1
+    	return profile_likelihood_maximization(U[:d], n_elbows, elbows_found.append(elbow + elbows_found[-1]))
+
+        """
+
