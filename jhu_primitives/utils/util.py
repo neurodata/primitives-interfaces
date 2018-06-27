475f9d5dc07e04ce3aa30bddae0effa8cd44cd9b
hhelm10 <31412718+hhelm10@users.noreply.github.com>
2018-06-27 16:09:28 -0400

    automatic .json generator (for primitives) added to utils

--- jhu_primitives/utils/util.py
+++ jhu_primitives/utils/util.py
@@ -3,6 +3,9 @@
 # util.py
 # Created on 2017-09-14.
 
+import os
+import d3m.index
+
 def file_path_conversion(abs_file_path, uri="file"):
     local_drive, file_path = abs_file_path.split(':')[0], abs_file_path.split(':')[1]
     path_sep = file_path[0]
@@ -28,3 +31,27 @@ def file_path_conversion(abs_file_path, uri="file"):
         return "file://localhost" + s
     else:
         return local_drive + ":" + s
+
+def generate_json():
+
+    version = "-1"
+
+    while version == "-1":
+        version = input("Please select API version. \n0 for v2018.1.26 \n1 for v2018.4.18 \n2 for v2018.6.5\n")
+        if version == "0":
+            version = "v2018.1.26"
+        elif version == "1":
+            version = "v2018.4.18"
+        elif version == "2":
+            version = "v2018.6.5"
+
+    path = os.path.abspath(os.getcwd()) + "\\primitives_repo\\" + version + "\\JHU\\"
+
+    for i in os.listdir(path):
+        temp = path + i
+        for j in os.listdir(temp): 
+            os.system('python -m d3m.index describe -i 4 ' + i + ' > ' + temp + '\\' + j + '\\primitive.json')
+
+if __name__ == '__main__':
+    generate_json()
+
