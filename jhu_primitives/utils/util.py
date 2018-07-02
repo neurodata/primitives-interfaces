603df53233617d58469b8abcb279d03fbf30238a
Joshua <jagterb1@jhu.edu>
2018-07-02 07:37:17 -0400

    utils changes

--- jhu_primitives/utils/util.py
+++ jhu_primitives/utils/util.py
@@ -8,6 +8,9 @@ import d3m.index
 
 def file_path_conversion(abs_file_path, uri="file"):
     local_drive, file_path = abs_file_path.split(':')[0], abs_file_path.split(':')[1]
+    if file_path is None:
+        file_path = local_drive
+        local_drive = None
     path_sep = file_path[0]
     file_path = file_path[1:]  # Remove initial separator
     if len(file_path) == 0:
@@ -29,6 +32,8 @@ def file_path_conversion(abs_file_path, uri="file"):
 
     if uri == "file":
         return "file://localhost" + s
+    elif local_drive is None:
+        return s
     else:
         return local_drive + ":" + s
 
