08791b2870ea421fefff934b46ef484506358072
Joshua <jagterb1@jhu.edu>
2018-07-02 08:09:40 -0400

    utils for linux

--- jhu_primitives/utils/util.py
+++ jhu_primitives/utils/util.py
@@ -7,10 +7,14 @@ import os
 import d3m.index
 
 def file_path_conversion(abs_file_path, uri="file"):
-    local_drive, file_path = abs_file_path.split(':')[0], abs_file_path.split(':')[1]
-    if file_path is None:
+    local_drive= abs_file_path.split(':')[0]
+
+    try:
+        file_path = abs_file_path.split(':')[1]
+    except IndexError:
         file_path = local_drive
         local_drive = None
+
     path_sep = file_path[0]
     file_path = file_path[1:]  # Remove initial separator
     if len(file_path) == 0:
