2c1c7f9b6a439d60e7ae8bfa3d2c9daf0fb73e5a
hhelm <hhelm2@jhu.edu>
2019-01-20 17:40:44 -0500

    updated jhu_primitives init

--- jhu_primitives/__init__.py
+++ jhu_primitives/__init__.py
@@ -4,36 +4,37 @@ from __future__ import absolute_import
 __all__ = ["AdjacencySpectralEmbedding", 
            #"AdjacencyMatrixConcatenator",
            "LaplacianSpectralEmbedding",
-           "DimensionSelection", 
+           #"DimensionSelection", 
            "GaussianClassification",
            "GaussianClustering", 
            "LargestConnectedComponent",
-           "NonParametricClustering",
-           "NumberOfClusters", 
-           "OutOfCoreAdjacencySpectralEmbedding", 
-           "PassToRanks",
+           #"NonParametricClustering",
+           #"NumberOfClusters", 
+           #"OutOfCoreAdjacencySpectralEmbedding", 
+           #"PassToRanks",
            "SpectralGraphClustering", 
            "SeededGraphMatching",
-           "VertexNominationSeededGraphMatching",
-           "SeededGraphMatchingPipeline"]
+           #"VertexNominationSeededGraphMatching",
+           #"SeededGraphMatchingPipeline"
+           ]
 
 from .ase import AdjacencySpectralEmbedding
 #from .adj_concat import AdjacencyMatrixConcatenator
 from .lse import LaplacianSpectralEmbedding
-from .dimselect import DimensionSelection
+#from .dimselect import DimensionSelection
 from .gclass import GaussianClassification
 from .gclust import GaussianClustering
 from .lcc import LargestConnectedComponent
-from .nonpar import NonParametricClustering
-from .numclust import NumberOfClusters
-from .oocase import OutOfCoreAdjacencySpectralEmbedding
-from .ptr import PassToRanks
+#from .nonpar import NonParametricClustering
+#from .numclust import NumberOfClusters
+#from .oocase import OutOfCoreAdjacencySpectralEmbedding
+#from .ptr import PassToRanks
 from .sgc import SpectralGraphClustering
 from .sgm import SeededGraphMatching
-from .vnsgm import VertexNominationSeededGraphMatching
+#from .vnsgm import VertexNominationSeededGraphMatching
 from .utils import file_path_conversion
 from .pipelines import sgm_pipeline
-from .pipelines import output_json
+#from .pipelines import output_json
 
 """
 __all__ = ['AdjacencySpectralEmbedding', 'LaplacianSpectralEmbedding',
