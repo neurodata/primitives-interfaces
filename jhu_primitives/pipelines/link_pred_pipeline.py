2c7dbb469126703aef6190f19911e27a31f95d2a
hhelm10 <31412718+hhelm10@users.noreply.github.com>
2019-04-29 16:59:49 -0400

    Create link_pred_pipeline.py

---
+++ jhu_primitives/pipelines/link_pred_pipeline.py
@@ -0,0 +1,81 @@
+from d3m.metadata import pipeline as meta_pipeline
+from d3m.metadata.base import Context, ArgumentType
+
+from jhu_primitives.pipelines.base import BasePipeline
+from jhu_primitives.link_pred_graph_reader import LinkPredictionGraphReader
+from jhu_primitives.ase import AdjacencySpectralEmbedding
+from jhu_primitives.link_pred_rc import LinkPredictionRankClassifier
+
+
+DATASETS = {
+    '59_umls'
+}
+
+
+class link_pred_pipeline(BasePipeline):
+    def __init__(self):
+        super().__init__(DATASETS)
+
+    def _gen_pipeline(self):
+        pipeline = meta_pipeline.Pipeline(context = Context.TESTING)
+        pipeline.add_input(name = 'inputs')
+
+        step_0 = meta_pipeline.PrimitiveStep(primitive_description = LinkPredictionGraphReader.metadata.query())
+        step_0.add_argument(
+            name = 'inputs',
+            argument_type=ArgumentType.CONTAINER,
+            data_reference='inputs.0'
+        )
+
+        step_0.add_output('produce')
+        pipeline.add_step(step_0)
+
+        step_1 = meta_pipeline.PrimitiveStep(primitive_description = AdjacencySpectralEmbedding.metadata.query())
+        step_1.add_argument(
+                name = 'inputs',
+                argument_type = ArgumentType.CONTAINER,
+                data_reference = 'steps.0.produce'
+        )
+        step_1.add_hyperparameter(
+                name = 'which_elbow',
+                argument_type = ArgumentType.VALUE,
+                data = 1
+        )
+        step_1.add_hyperparameter(
+                name = 'max_dimension',
+                argument_type = ArgumentType.VALUE,
+                data = 2
+        )
+        step_1.add_hyperparameter(
+                name = 'use_attributes',
+                argument_type = ArgumentType.VALUE,
+                data = False
+        )
+
+        step_1.add_output('produce')
+        pipeline.add_step(step_1)
+
+
+        step_2 = meta_pipeline.PrimitiveStep(primitive_description = LinkPredictionRankClassifier.metadata.query())
+        step_2.add_argument(
+            name = 'inputs',
+            argument_type= ArgumentType.CONTAINER,
+            data_reference=  'steps.1.produce'
+        )
+        step_2.add_hyperparameter(
+                name = 'max_clusters',
+                argument_type = ArgumentType.VALUE,
+                data = 10
+        )
+
+        step_2.add_output('produce')
+        pipeline.add_step(step_2)
+
+        # Adding output step to the pipeline
+        pipeline.add_output(name = 'Predictions', data_reference = 'steps.2.produce')
+
+        return pipeline
+
+    def assert_result(self, tester, results, dataset):
+        tester.assertEquals(len(results), 1)
+        #tester.assertEquals(len(results[0]), 1208)
