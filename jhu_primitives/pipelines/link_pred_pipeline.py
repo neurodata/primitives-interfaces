84724697dede6215bd18da56d9cb47d08188bfff
hhelm <hhelm2@jhu.edu>
2019-06-15 11:44:04 -0400

    remove context argument from pipelines

--- jhu_primitives/pipelines/link_pred_pipeline.py
+++ jhu_primitives/pipelines/link_pred_pipeline.py
@@ -17,7 +17,7 @@ class link_pred_pipeline(BasePipeline):
         super().__init__(DATASETS)
 
     def _gen_pipeline(self):
-        pipeline = meta_pipeline.Pipeline(context = Context.TESTING)
+        pipeline = meta_pipeline.Pipeline()
         pipeline.add_input(name = 'inputs')
 
         step_0 = meta_pipeline.PrimitiveStep(primitive_description = DataConversion.metadata.query())
