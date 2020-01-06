ca778aa5540f21dbb1bc6f8646f66bddd0792c76
hh <hhelm2@jhu.edu>
2020-01-06 20:18:34 +0000

    checking out keys, changing inputs

--- jhu_primitives/lcc/lcc.py
+++ jhu_primitives/lcc/lcc.py
@@ -88,8 +88,10 @@ class LargestConnectedComponent(TransformerPrimitiveBase[Inputs, Outputs, Hyperp
 
         """
         print(dir(inputs), file=sys.stderr)
-        print(inputs.values(), file=sys.stderr)
-        print(inputs.to_json_structure(), file=sys.stderr)
+        print(inputs.keys(), file=sys.stderr)
+        # print(inputs.values(), file=sys.stderr)
+        inputs=inputs.values()
+        # print(inputs.to_json_structure(), file=sys.stderr)
         try:
             G = inputs['0']
         except:
