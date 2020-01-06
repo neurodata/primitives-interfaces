9655249de5ed233e88c7aff8923b525f93507b06
hh <hhelm2@jhu.edu>
2020-01-06 20:22:05 +0000

    what are the items

--- jhu_primitives/lcc/lcc.py
+++ jhu_primitives/lcc/lcc.py
@@ -88,9 +88,10 @@ class LargestConnectedComponent(TransformerPrimitiveBase[Inputs, Outputs, Hyperp
 
         """
         print(dir(inputs), file=sys.stderr)
+        print(inputs.items(), file=sys.stderr)
         print(inputs.keys(), file=sys.stderr)
         # print(inputs.values(), file=sys.stderr)
-        inputs=inputs.values()
+        # inputs=inputs.values()
         # print(inputs.to_json_structure(), file=sys.stderr)
         try:
             G = inputs['0']
