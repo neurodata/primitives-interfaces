9d54a65703322597d7b5f4c6eca3ba2081600a98
hh <hhelm2@jhu.edu>
2020-01-08 21:57:27 +0000

    try

--- jhu_primitives/link_pred_graph_reader/link_pred_graph_reader.py
+++ jhu_primitives/link_pred_graph_reader/link_pred_graph_reader.py
@@ -86,7 +86,8 @@ class LinkPredictionGraphReader(TransformerPrimitiveBase[Inputs, Outputs, Hyperp
         
         print(graph, file=sys.stderr)
         print(dir(inputs), file=sys.stderr)
-        print(inputs.item(), file=sys.stderr)
+        print(inputs.items(), file=sys.stderr)
+        print(inputs.location_uri, file=sys.stderr)
         n = len(graph)
 
         # grab link types (values) and edge list (keys)
