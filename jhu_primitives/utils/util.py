b43edbc0fee6fda807329fefe62292a8619409e2
hh <hhelm2@jhu.edu>
2020-01-08 20:54:16 +0000

    cora

--- jhu_primitives/utils/util.py
+++ jhu_primitives/utils/util.py
@@ -48,9 +48,9 @@ DATASETS = {
                 # "LL1_imports_net"
                 # ],
             "vertexNomination_class": [
-                "datasets/training_datasets/seed_datasets_archive/LL1_net_nomination_seed",
+                # "datasets/training_datasets/seed_datasets_archive/LL1_net_nomination_seed",
                 # "datasets/training_datasets/seed_datasets_archive/LL1_EDGELIST_net_nomination_seed",
-                # "LL1_VTXC_1343_cora",
+                "datasets/training_datasets/seed_datasets_archive/LL1_VTXC_1343_cora",
                 # "LL1_VTXC_1369_synthetic",
                 ],
             # "vertexNomination_clust": [
