5ee94f294bfb49f9b8c4f0b43a3861c16f4c8593
hh <hhelm2@jhu.edu>
2020-01-09 17:17:26 +0000

    is it a graph?

--- jhu_primitives/link_pred_graph_reader/link_pred_graph_reader.py
+++ jhu_primitives/link_pred_graph_reader/link_pred_graph_reader.py
@@ -86,7 +86,8 @@ class LinkPredictionGraphReader(TransformerPrimitiveBase[Inputs, Outputs, Hyperp
         print(dir(inputs), file=sys.stderr)
         for i in inputs:
             print(i, file=sys.stderr)
-        print(inputs[0], file=sus.stderr)
+        print(type(inputs[0]), files=sys.stderr)
+        print(inputs[0].edges.data(), file=sys.stderr)
 
         temp_json = inputs.to_json_structure()
         location_uri = temp_json['location_uris'][0]
