db38cf97e4b1a98cb094a029e58e646c7b02e791
root <bvarjavand@gmail.com>
2020-01-15 16:17:49 +0000

    add dataset to dataframe

--- jhu_primitives/pipelines/gmm_ase_pipeline.py
+++ jhu_primitives/pipelines/gmm_ase_pipeline.py
@@ -1,5 +1,7 @@
 from d3m.metadata import pipeline as meta_pipeline
 from d3m.metadata.base import Context, ArgumentType
+import common_primitives
+from common_primitives import DatasetToDataFramePrimitive
 
 from jhu_primitives.pipelines.base import BasePipeline
 from jhu_primitives.ase import AdjacencySpectralEmbedding
@@ -21,7 +23,7 @@ class gmm_ase_pipeline(BasePipeline):
         pipeline = meta_pipeline.Pipeline()
         pipeline.add_input(name = 'inputs')
 
-        step_0 = meta_pipeline.PrimitiveStep(primitive_description=GraphReader.metadata.query())
+        step_0 = meta_pipeline.PrimitiveStep(primitive_description=DatasetToDataFramePrimitive.metadata.query())
         step_0.add_argument(
             name = 'inputs',
             argument_type=ArgumentType.CONTAINER,
@@ -31,7 +33,7 @@ class gmm_ase_pipeline(BasePipeline):
         step_0.add_output('produce')
         pipeline.add_step(step_0)
 
-        step_1 = meta_pipeline.PrimitiveStep(primitive_description=LargestConnectedComponent.metadata.query())
+        step_1 = meta_pipeline.PrimitiveStep(primitive_description=GraphReader.metadata.query())
         step_1.add_argument(
             name = 'inputs',
             argument_type=ArgumentType.CONTAINER,
@@ -41,44 +43,54 @@ class gmm_ase_pipeline(BasePipeline):
         step_1.add_output('produce')
         pipeline.add_step(step_1)
 
-        step_2 = meta_pipeline.PrimitiveStep(primitive_description = AdjacencySpectralEmbedding.metadata.query())
+        step_2 = meta_pipeline.PrimitiveStep(primitive_description=LargestConnectedComponent.metadata.query())
         step_2.add_argument(
-                name = 'inputs',
-                argument_type = ArgumentType.CONTAINER,
-                data_reference = 'steps.1.produce'
-        )
-        step_2.add_hyperparameter(
-                name = 'max_dimension',
-                argument_type = ArgumentType.VALUE,
-                data = 5
-        )
-        step_2.add_hyperparameter(
-                name = 'use_attributes',
-                argument_type = ArgumentType.VALUE,
-                data = True
+            name = 'inputs',
+            argument_type=ArgumentType.CONTAINER,
+            data_reference='steps.1.produce'
         )
 
         step_2.add_output('produce')
         pipeline.add_step(step_2)
 
-
-        step_3 = meta_pipeline.PrimitiveStep(primitive_description= GaussianClustering.metadata.query())
+        step_3 = meta_pipeline.PrimitiveStep(primitive_description = AdjacencySpectralEmbedding.metadata.query())
         step_3.add_argument(
-            name = 'inputs',
-            argument_type= ArgumentType.CONTAINER,
-            data_reference=  'steps.2.produce'
+                name = 'inputs',
+                argument_type = ArgumentType.CONTAINER,
+                data_reference = 'steps.2.produce'
         )
         step_3.add_hyperparameter(
-                name = 'max_clusters',
+                name = 'max_dimension',
                 argument_type = ArgumentType.VALUE,
-                data = 10
+                data = 5
+        )
+        step_3.add_hyperparameter(
+                name = 'use_attributes',
+                argument_type = ArgumentType.VALUE,
+                data = True
         )
 
         step_3.add_output('produce')
         pipeline.add_step(step_3)
 
+
+        step_4 = meta_pipeline.PrimitiveStep(primitive_description= GaussianClustering.metadata.query())
+        step_4.add_argument(
+            name = 'inputs',
+            argument_type= ArgumentType.CONTAINER,
+            data_reference=  'steps.3.produce'
+        )
+        step_4.add_hyperparameter(
+                name = 'max_clusters',
+                argument_type = ArgumentType.VALUE,
+                data = 10
+        )
+
+        step_4.add_output('produce')
+        pipeline.add_step(step_4)
+
         # Adding output step to the pipeline
-        pipeline.add_output(name = 'Predictions', data_reference = 'steps.3.produce')
+        pipeline.add_output(name = 'Predictions', data_reference = 'steps.4.produce')
 
         return pipeline
 
