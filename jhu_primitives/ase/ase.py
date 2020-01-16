1f6551726d62e0e80a5f4a02148d13c57306a930
Anton Alyakin <alyakin314@gmail.com>
2020-01-16 16:10:02 -0500

    forgot csv

--- jhu_primitives/ase/ase.py
+++ jhu_primitives/ase/ase.py
@@ -127,6 +127,7 @@ class AdjacencySpectralEmbedding(TransformerPrimitiveBase[Inputs, Outputs, Hyper
     def produce(self, *, inputs: Inputs, timeout: float = None, iterations: int = None) -> CallResult[Outputs]:
         np.random.seed(1234)
         print('ase, baby!', file=sys.stderr)
+        csv = inputs[0]
         G = inputs[1][0].copy()
 
         headers=csv.columns
