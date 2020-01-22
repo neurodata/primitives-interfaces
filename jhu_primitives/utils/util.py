624d69ba044d81cc7b33fdee60e08dc319a64a9b
A <aalyaki1@jhu.edu>
2020-01-22 18:32:07 +0000

    carefully chagned utilspy

--- jhu_primitives/utils/util.py
+++ jhu_primitives/utils/util.py
@@ -14,8 +14,8 @@ import re
 
 PROBLEM_TYPES = [
     # "graphMatching",
-    "vertexClassification",
-    # "linkPrediction",
+    # "vertexClassification",
+    "linkPrediction",
     # "communityDetection"
     ]
 
@@ -23,19 +23,19 @@ DATASETS = {
             # "graphMatching": [
             #     "datasets-private/seed_datasets_current/49_facebook_MIN_METADATA",
             #     ],
-            "vertexClassification": [
-                "datasets/seed_datasets_current/LL1_net_nomination_seed_MIN_METADATA",
-                "datasets/seed_datasets_current/LL1_EDGELIST_net_nomination_seed_MIN_METADATA",
-                "datasets/seed_datasets_current/LL1_VTXC_1343_cora_MIN_METADATA",
-                "datasets/seed_datasets_current/LL1_VTXC_1369_synthetic_MIN_METADATA",
-                ],
+            # "vertexClassification": [
+            #     "datasets/seed_datasets_current/LL1_net_nomination_seed_MIN_METADATA",
+            #     "datasets/seed_datasets_current/LL1_EDGELIST_net_nomination_seed_MIN_METADATA",
+            #     "datasets/seed_datasets_current/LL1_VTXC_1343_cora_MIN_METADATA",
+            #     "datasets/seed_datasets_current/LL1_VTXC_1369_synthetic_MIN_METADATA",
+            #     ],
             # "communityDetection": [
             #     "datasets/seed_datasets_current/LL1_bn_fly_drosophila_medulla_net_MIN_METADATA",
             #     ],
-            # "linkPrediction": [
-            #     "datasets/seed_datasets_current/59_umls_MIN_METADATA",
-            #     "datasets/seed_datasets_current/59_LP_karate_MIN_METADATA"
-            #     ]
+            "linkPrediction": [
+                "datasets/seed_datasets_current/59_umls_MIN_METADATA",
+                "datasets/seed_datasets_current/59_LP_karate_MIN_METADATA"
+                ]
             }
 
 PIPELINES = {
@@ -43,13 +43,13 @@ PIPELINES = {
             #     "sgm_pipeline",
             #     "sgm_pipeline_10"
             #     ],
-            "vertexClassification": [
-                "gclass_ase_pipeline",
+            # "vertexClassification": [
+            #     "gclass_ase_pipeline",
             #     "gclass_lse_pipeline",
             #     "gclass_oosase_pipeline",
             #     "gclass_ooslse_pipeline",
             #     "sgc_pipeline"
-                ],
+            #     ],
             # "communityDetection": [
             #     "gmm_ase_pipeline",
             #     "gmm_lse_pipeline",
@@ -57,9 +57,9 @@ PIPELINES = {
             #     "gmm_ooslse_pipeline"
             #     "sgc_pipeline"
             #     ],
-            # "linkPrediction": [
-            #     "link_pred_pipeline",
-            #     ],
+            "linkPrediction": [
+                "link_pred_pipeline",
+                ],
              }
 
 def convert(name):
