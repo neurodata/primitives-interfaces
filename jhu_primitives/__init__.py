38b69529d5710ca3102ee97bff13961b0d77a9c5
Anton Alyakin <alyakin314@gmail.com>
2020-06-09 17:50:36 +0300

    modified init to reflect removed primitives

--- jhu_primitives/__init__.py
+++ jhu_primitives/__init__.py
@@ -1,32 +1,27 @@
 from __future__ import absolute_import
 
 __all__ = [
-           "AdjacencySpectralEmbedding",
            "LoadGraphs",
-           "LaplacianSpectralEmbedding",
-           "GaussianClassification",
-           "GaussianClustering",
            "LargestConnectedComponent",
-           "LinkPredictionGraphReader",
-           "LinkPredictionRankClassifier",
+           "AdjacencySpectralEmbedding",
+           "LaplacianSpectralEmbedding",
            # "OutOfSampleAdjacencySpectralEmbedding",
            # "OutOfSampleLaplacianSpectralEmbedding",
-           "SingleGraphVertexNomination",
-           "SpectralGraphClustering",
+           "GaussianClassification",
+           "GaussianClustering",
+           "LinkPredictionGraphReader",
+           "LinkPredictionRankClassifier",
            "SeededGraphMatching",
            ]
 
-from .ase import AdjacencySpectralEmbedding
-from .load_graphs import LoadGraphs 
-from .lse import LaplacianSpectralEmbedding
-from .gclass import GaussianClassification
-from .gclust import GaussianClustering
-# from .graph_reader import GraphReader
+from .load_graphs import LoadGraphs
 from .lcc import LargestConnectedComponent
-from .link_pred_graph_reader import LinkPredictionGraphReader
-from .link_pred_rc import LinkPredictionRankClassifier
+from .ase import AdjacencySpectralEmbedding
+from .lse import LaplacianSpectralEmbedding
 # from .oosase import OutOfSampleAdjacencySpectralEmbedding
 # from .ooslse import OutOfSampleLaplacianSpectralEmbedding
-from .sgc import SpectralGraphClustering
+from .gclass import GaussianClassification
+from .gclust import GaussianClustering
+from .link_pred_graph_reader import LinkPredictionGraphReader
+from .link_pred_rc import LinkPredictionRankClassifier
 from .sgm import SeededGraphMatching
-from .sgvn import SingleGraphVertexNomination
