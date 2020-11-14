170cb883726544a50564276df881de7d60d4801d
Anton Alyakin <alyakin314@gmail.com>
2020-11-14 12:35:30 -0500

    only euclid

--- jhu_primitives/pipelines/euclidean_nomination_pipeline.py
+++ jhu_primitives/pipelines/euclidean_nomination_pipeline.py
@@ -20,19 +20,35 @@ class euclidean_nomination_pipeline(BasePipeline):
 
         # Step 0: dataset_to_dataframe
         step_0 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.dataset_to_dataframe.Common'))
-        step_0.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='inputs.0')
+        step_0.add_argument(name='inputs',
+                            argument_type=ArgumentType.CONTAINER,
+                            data_reference='inputs.0')
         step_0.add_output('produce')
         pipeline.add_step(step_0)
 
         # Step 1: dataset_to_dataframe
         step_1 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.dataset_to_dataframe.Common'))
-        step_1.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='inputs.1')
+        step_1.add_argument(name='inputs',
+                            argument_type=ArgumentType.CONTAINER,
+                            data_reference='inputs.0')
+        step_1.add_hyperparameter(
+                name='dataframe_resource',
+                argument_type = ArgumentType.VALUE,
+                data='1'
+        )
         step_1.add_output('produce')
         pipeline.add_step(step_1)
 
         # Step 2: dataset_to_dataframe
         step_2 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.dataset_to_dataframe.Common'))
-        step_2.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='inputs.2')
+        step_2.add_argument(name='inputs',
+                            argument_type=ArgumentType.CONTAINER,
+                            data_reference='inputs.0')
+        step_2.add_hyperparameter(
+                name='dataframe_resource',
+                argument_type=ArgumentType.VALUE,
+                data='2'
+        )
         step_2.add_output('produce')
         pipeline.add_step(step_2)
 
