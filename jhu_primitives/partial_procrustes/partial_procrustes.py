7bd4f4d8234bdcedbd9c8c2818ba73a1698e41c3
Ali Saad-Eldin <ali.saadeldin11@gmail.com>
2021-02-03 17:45:39 -0500

    Update partial_procrustes.py

--- jhu_primitives/partial_procrustes/partial_procrustes.py
+++ jhu_primitives/partial_procrustes/partial_procrustes.py
@@ -10,7 +10,9 @@ import numpy as np
 
 from scipy.linalg import orthogonal_procrustes
 
+from d3m import utils
 from d3m import container
+from d3m import exceptions
 from d3m.primitive_interfaces.unsupervised_learning import UnsupervisedLearnerPrimitiveBase
 from d3m.metadata import hyperparams, base as metadata_module, params
 from d3m.primitive_interfaces import base
