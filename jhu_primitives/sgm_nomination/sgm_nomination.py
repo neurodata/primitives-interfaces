bad12b7613d71a6eddb6159f063cfa1d9ed1f945
Ali Saad-Eldin <ali.saadeldin11@gmail.com>
2021-03-09 13:44:54 -0500

    Update sgm_nomination.py

--- jhu_primitives/sgm_nomination/sgm_nomination.py
+++ jhu_primitives/sgm_nomination/sgm_nomination.py
@@ -124,9 +124,9 @@ class SgmNomination(UnsupervisedLearnerPrimitiveBase[Inputs, Outputs, Params, Hy
         x_seeds = np.zeros(n_seeds)
         y_seeds = np.zeros(n_seeds)
         for i in range(n_seeds):
-            x_seeds[i] = (xhat[xhat.columns[0]] == xhat_seed_names[i]).nonzero()[0][0]
+            x_seeds[i] = np.where(xhat[xhat.columns[0]] == xhat_seed_names[i])[0][0]
 
-            y_seeds[i] = (yhat[yhat.columns[0]] == yhat_seed_names[i]).nonzero()[0][0]
+            y_seeds[i] = np.where(yhat[yhat.columns[0]] == yhat_seed_names[i])[0][0]
 
 
         # do this more carefully TODO
