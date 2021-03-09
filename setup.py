6d09711e8cb60c88f61f3a7bad1cfedb0b0abdea
Anton Alyakin <alyakin314@gmail.com>
2021-03-09 18:41:52 +0300

    alis bug fixed

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ setup(
                       'graspy>=0.0.2',
                       'graspologic',
                       'numba>=0.52.0',
-                      'hyppo==1.3.0',
+                      'hyppo==0.1.3',
 
 
                       # Begin d3m dependency
