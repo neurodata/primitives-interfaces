35ed2ad4503f4207aab9601b11fad1598849383a
Anton Alyakin <alyakin314@gmail.com>
2021-03-10 16:40:40 +0300

    added print statements to debug

--- jhu_primitives/sgm_nomination/sgm_nomination.py
+++ jhu_primitives/sgm_nomination/sgm_nomination.py
@@ -116,6 +116,11 @@ class SgmNomination(UnsupervisedLearnerPrimitiveBase[Inputs, Outputs, Params, Hy
         yhat = self._inputs_2
 
         seeds = self._reference['match'].astype(bool)
+        print(len(self._referencee), file=sys.stderr)
+        print(self._reference.head(5), file=sys.stderr)
+        print(seeds, file=sys.stderr)
+        print(type(seeds), file=sys.stderr)
+        print(len(seeds), file=sys.stderr)
 
         xhat_seed_names = self._reference[self._reference.columns[1]][seeds].values
         yhat_seed_names = self._reference[self._reference.columns[2]][seeds].values
